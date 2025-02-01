1714ba2f81d492616c00a401ead9846fed7b01c2
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2025-02-01 18:52:59 +0530

    Updating All files with updated routes

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import Hero from './Hero';
-import Awards from './Awards';
-import Stats from './Stats';
-import Pricing from './Pricing';
-import Education from './Education';
-import OpenAccount from '../OpenAccount';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
+import React from "react";
+import Hero from "./Hero";
+import Awards from "./Awards";
+import Stats from "./Stats";
+import Pricing from "./Pricing";
+import Education from "./Education";
+
+import OpenAccount from "../OpenAccount";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
 function HomePage() {
-    return ( 
-
+  return (
     <>
-        <Navbar />
-        < Hero />
-        <Awards />
-        <Stats />
-        <Pricing />
-        <Education />
-        <OpenAccount />
-        <Footer />
-
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <OpenAccount />
     </>
-
-     );
+  );
 }
 
 export default HomePage;
--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
-function SupportPage() {
-    return ( 
-        <h1>SupportPage</h1>
-     );
+import Hero from "./Hero";
+import CreateTicket from "./CreateTicket";
+
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <CreateTicket />
+    </>
+  );
 }
 
-export default SupportPage;
+export default PricingPage;
