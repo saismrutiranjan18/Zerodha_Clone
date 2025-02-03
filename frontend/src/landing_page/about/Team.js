5ccb5e064210d2b96aadd60713197421c1196bc9
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2025-02-03 22:56:10 +0530

    Updating files

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -12,22 +12,17 @@ function Team() {
         style={{ lineHeight: "1.8", fontSize: "1.2em" }}
       >
         <div className="col-6 p-3 text-center">
-          <img
-            src="media/images/main.jpg"
-            style={{ borderRadius: "100%", width: "50%" }}
-          />
-          <h4 className="mt-5">Sudarshan Patil</h4>
+        <img src="/media/images/saiSmruti.jpg" style={{ borderRadius: "100%", width: "50%" }} />
+
+          <h4 className="mt-5">Sai Smruti</h4>
           <h6>Front End Developer</h6>
         </div>
         <div className="col-6 p-3">
-          <p>My name is Sudarshan Patil, a developer with 1.6 years of experience as a survey programmer and 7 months of freelancing experience. I hold a B.Tech in Electronics and Telecommunication.</p>
-          <p>To enhance my resume and showcase my full-stack development skills, I developed a Zerodha clone. This project involved both frontend and backend technologies, including HTML, CSS, JavaScript, React, Bootstrap, Node.js, Express.js, MongoDB, and React Router DOM. It demonstrates my ability to create comprehensive, functional, and responsive web applications.</p>
-          <p>I'm excited to continue applying my skills in front-end and back-end development and contributing to impactful projects.</p>
+          <p>My name is Sai Smruti Ranjan Das, and I am a passionate Full Stack Developer currently pursuing a B.Tech in Computer Science and Engineering.</p>
+          <p>I have built projects like Tiffin Fusion, an Airbnb clone, and a weather website using React and APIs. As a contributor in GSSoC and Hacktoberfest, I actively participate in open-source projects.</p>
+          <p>I am eager to gain industry experience through internships and contribute to innovative projects while continuously improving my technical and problem-solving abilities in web development.</p>
           <p className='text-center'>
-            Connect on <a href="www.linkedin.com/in/mr-sudarshan"><i className="fa-brands fa-linkedin text-body-secondary fs-5"></i></a>&nbsp;&nbsp; <a href="https://www.instagram.com/_sudarshan.patil_/"><i className="fa-brands fa-instagram text-body-secondary fs-5"></i></a> 
-          </p>
-          <p className='text-center'>
-           <a href="https://mr-sudarshan-patil.github.io/Sp-Dev/" className='text-body-secondary fs-5'>Portfolio</a>
+            Connect on <a href="https://www.linkedin.com/in/saismrutiranjan/"><i className="fa-brands fa-linkedin text-body-secondary fs-5"></i></a>&nbsp;&nbsp; <a href="https://github.com/saismrutiranjan18"><i className="fa-brands fa-github text-body-secondary fs-5"></i></a> 
           </p>
         </div>
       </div>
