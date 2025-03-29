ccead6af27c404d6eeb85c0c183be00fca1c2803
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2025-03-29 11:52:30 +0530

    updating

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002;
+const url = process.env.MONGODB_URL;
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
+
+  mongoose.connect(url);
+  console.log("Connected to MongoDB");
 });
