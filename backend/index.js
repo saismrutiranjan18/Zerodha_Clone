1901a39897453a121433af4b22c201a105449d1c
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2025-06-22 22:54:45 +0530

    Update code

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,21 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const { HoldingsModel } = require("./model/HoldingsModel");
 
 const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
 
 const PORT = process.env.PORT || 3002;
-const url = process.env.MONGODB_URL;
+const uri = process.env.MONGO_URL;
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // app.get("/addHoldings", async (req, res) => {
 //   let tempHoldings = [
@@ -181,9 +187,43 @@ const app = express();
 //   res.send("Done!");
 // });
 
+
+app.get("/allHoldings", async (req, res) => {
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings);
+});
+
+
+app.get("/allPositions", async (req, res) => {
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("Order saved!");
+});
+
+
+
 app.listen(PORT, () => {
   console.log("Server is running on port 3002");
 
   mongoose.connect(url);
   console.log("Connected to MongoDB");
 });
+
+
+
+
+
+
+//npm start for starting the server
